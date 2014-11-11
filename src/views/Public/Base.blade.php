41cfe34ce238aa415b82c23db2d57d15b6d1f017
Tom Green <kingofcuddles@gmail.com>
2014-11-11 18:17:21 +0000

    working on base templates.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/views/Public/Base.blade.php
+++ src/views/Public/Base.blade.php
@@ -16,44 +16,15 @@
 
     <!-- Le styles -->
     <link rel="stylesheet" href="{{ url() }}/css/bootstrap.min.css">
-
-    <style>
-      body {
-        margin-top: 50px;
-      }
-    </style>
   </head>
 
   <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="{{ url() }}">{SiteName}</a>
-            <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="container white">
-      @yield('content')
+    @include('core::Public.Partials.Navigation')
 
-      <div class="row">
-        <div class="span12">
-          <p>&copy; Company 2012</p>
-        </div>
-      </div>
-    </div>
+    @yield('content')
+
+    @include('core::Public.Partials.Footer')
 
     <!-- Le javascript -->
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
---
+++ src/views/Public/Partials/Footer.blade.php
@@ -0,0 +1,4 @@
+<div class="container">
+  <hr />
+  <p class="text-muted text-center">&copy; 2013 Company Name. All rights reserved</p>
+</div>
